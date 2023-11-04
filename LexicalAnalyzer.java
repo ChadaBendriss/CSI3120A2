3a0d9e068164e5103eb2acb98186bdd921211365
ChadaBendriss <113207156+ChadaBendriss@users.noreply.github.com>
2023-11-04 12:16:34 -0400

    A2 First commit

---
+++ LexicalAnalyzer.java
@@ -0,0 +1,344 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LexicalAnalyzer {
+    /* Global declarations */
+    /* Variables */
+    public static int charClass;
+    public static StringBuilder lexeme = new StringBuilder(100);
+    public static StringBuilder error = new StringBuilder(100);
+    public static char nextChar;
+    public static int lexLen;
+    public static int token;
+    public static int nextToken;
+    public static BufferedReader in_fp;
+
+    /* Character classes */
+    public static final int EOF = -1;
+    public static final int LETTER = 0;
+    public static final int DIGIT = 1;
+    public static final int UNDERSCORE = 2;
+    public static final int UNKNOWN = 99;
+
+    /* Token codes */
+    public static final int FLOAT_LIT = 12;
+    public static final int IDENT = 11;
+    public static final int INT_LIT = 10;
+    public static final int STR_LIT = 13;
+    public static final int ASSIGN_OP = 20;
+    public static final int ADD_OP = 21;
+    public static final int SUB_OP = 22;
+    public static final int MULT_OP = 23;
+    public static final int DIV_OP = 24;
+    public static final int LEFT_PAREN = 25;
+    public static final int RIGHT_PAREN = 26;
+    public static final int LEFT_BRACE = 27;
+    public static final int RIGHT_BRACE = 28;
+    public static final int SEMICOLON = 29;
+    public static final int LESS_THAN = 30;
+    public static final int GREATER_THAN = 31;
+    public static final int EQUALS = 32;
+    public static final int NOT_EQUALS = 33;
+    public static final int AND_OP = 34;
+    public static final int OR_OP = 35;
+    public static final int IF = 36;
+    public static final int ELSE = 37;
+    public static final int FOR = 38;
+    public static final int WHILE = 39;
+    public static final int COMMENT = 40;
+    public static final int QUESTION_MARK = 41;
+    public static final int COLON = 42;
+    public static final int LEFT_BRACKET = 43; 
+    public static final int RIGHT_BRACKET = 44; 
+   
+    public static int lineNum = 1;  // start from 1 since we start at the first line
+
+
+
+    public static void main(String[] args) {
+        /* Open the input data file and process its contents */
+        try {
+            in_fp = new BufferedReader(new FileReader("input20.txt"));
+            getChar();
+            do {
+                lex();
+                
+            } while (nextToken != EOF);
+        } catch (IOException e) {
+            System.err.println("ERROR - cannot open front.in");
+        }
+    }
+
+    /* lookup - a function to lookup operators and keywords and return the token */
+    public static void lookup(char ch) throws IOException {
+        switch (ch) {
+            case '(':
+                addChar();
+                nextToken = LEFT_PAREN;
+                break;
+            case ')':
+                addChar();
+                nextToken = RIGHT_PAREN;
+                break;
+            case '{':
+                addChar();
+                nextToken = LEFT_BRACE;
+                break;
+            case '}':
+                addChar();
+                nextToken = RIGHT_BRACE;
+                break;
+            case '+':
+                addChar();
+                nextToken = ADD_OP;
+                break;
+            case '-':
+                addChar();
+                nextToken = SUB_OP;
+                break;
+            case '*':
+                addChar();
+                nextToken = MULT_OP;
+                break;
+            case '/':
+                addChar();
+                getChar();
+                nextToken = isComment() ? COMMENT : DIV_OP;
+                break;
+            case '=':
+                addChar();
+                getChar();
+                if (nextChar == '=') {
+                    addChar();
+                    nextToken = EQUALS;
+                } else {
+                    nextToken = ASSIGN_OP;
+                }
+                break;
+            case ';':
+                addChar();
+                nextToken = SEMICOLON;
+                break;
+            case '<':
+                addChar();
+                getChar();
+                if (nextChar == '=') {
+                    addChar();
+                    nextToken = LESS_THAN;
+                } else {
+                    nextToken = LESS_THAN;
+                }
+                break;
+            case '>':
+                addChar();
+                getChar();
+                if (nextChar == '=') {
+                    addChar();
+                    nextToken = GREATER_THAN;
+                } else {
+                    nextToken = GREATER_THAN;
+                }
+                break;
+            case '!':
+                addChar();
+                getChar();
+                if (nextChar == '=') {
+                    addChar();
+                    nextToken = NOT_EQUALS;
+                } else {
+                    nextToken = UNKNOWN;
+                }
+                break;
+            case '&':
+                addChar();
+                getChar();
+                if (nextChar == '&') {
+                    addChar();
+                    nextToken = AND_OP;
+                } else {
+                    nextToken = UNKNOWN;
+                }
+                break;
+            case '|':
+                addChar();
+                getChar();
+                if (nextChar == '|') {
+                    addChar();
+                    nextToken = OR_OP;
+                } else {
+                    nextToken = UNKNOWN;
+                }
+                break;
+            case '?':
+                addChar();
+                nextToken = QUESTION_MARK;
+                break;
+            case ':':
+                addChar();
+                nextToken = COLON;
+                break;
+            case '\"':
+                addChar();
+                getChar();
+                while (nextChar != '\"') {
+                    addChar();
+                    getChar();
+                }
+                addChar(); // Include the closing double quote
+                
+                nextToken = STR_LIT;
+                break;
+            default:
+                addChar();
+                nextToken = EOF;
+                break;
+        }
+    }
+
+    public static boolean isComment() throws IOException {
+        if (nextChar == '/') {
+            // This is the beginning of a single-line comment
+            while (nextChar != '\n')
+                getChar(); // Ignore characters in the comment
+            nextToken = COMMENT;
+            lexeme = new StringBuilder("a single line comment");
+        } else if (nextChar == '*') {
+            // This is the beginning of a block comment
+            addChar();
+            getChar();
+            while (!(nextChar == '*' && in_fp.read() == '/')) {
+                getChar();
+            }
+            getChar(); // Consume the '/'
+            nextToken = COMMENT;
+            lexeme = new StringBuilder("a block comment");
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    /* addChar - a function to add nextChar to lexeme */
+    public static void addChar() {
+        if (lexLen <= 98) {
+            lexeme.append(nextChar);
+        } else {
+            System.err.println("Error - lexeme is too long");
+        }
+    }
+
+    /* getChar - a function to get the next character of input and determine its character class */
+    public static void getChar() throws IOException {
+        int nextCharInt = in_fp.read();
+        if (nextCharInt != -1) {
+            nextChar = (char) nextCharInt;
+            if (Character.isLetter(nextChar))
+                charClass = LETTER;
+            else if (nextChar == '_')
+                charClass = UNDERSCORE;
+            else if (Character.isDigit(nextChar))
+                charClass = DIGIT;
+            else
+                charClass = UNKNOWN;
+        } else {
+            charClass = EOF;
+            //System.out.println('k');
+        }
+        if (nextChar == '\n') {
+            lineNum++;
+        }
+    }
+
+    /* getNonBlank - a function to call getChar until it returns a non-whitespace character */
+    public static void getNonBlank() throws IOException {
+        
+        while (Character.isWhitespace(nextChar) && charClass != EOF)
+        
+            getChar();
+            System.out.println(nextChar);
+    }
+
+    /* lex - a simple lexical analyzer for arithmetic expressions */
+    public static void lex() throws IOException {
+        lexLen = 0;
+        lexeme = new StringBuilder();
+        getNonBlank();
+        String specialChar = "(+-/*<>)";
+        System.out.println("charclass "+charClass);
+
+        switch (charClass) {
+            /* Parse identifiers or keywords */
+            case LETTER:
+            case UNDERSCORE:
+                addChar();
+                getChar();
+                while (charClass == LETTER || charClass == DIGIT || charClass == UNDERSCORE) {
+                    addChar();
+                    getChar();
+                }
+                // Check if the lexeme is a keyword
+                String lexemeStr = lexeme.toString();
+                if (lexemeStr.equals("if"))
+                    nextToken = IF;
+                else if (lexemeStr.equals("else"))
+                    nextToken = ELSE;
+                else if (lexemeStr.equals("for"))
+                    nextToken = FOR;
+                else if (lexemeStr.equals("while"))
+                    nextToken = WHILE;
+                else if (charClass == UNKNOWN && !Character.isWhitespace(nextChar) && !specialChar.contains(String.valueOf(nextChar))) {
+                    addChar();
+                    error = new StringBuilder("Error - illegal identifier");
+                    nextToken = EOF;
+                } else
+                    nextToken = IDENT;
+                break;
+            // Parse integer literals
+            case DIGIT:
+                addChar();
+                getChar();
+                while (charClass == DIGIT) {
+                    addChar();
+                    getChar();
+                }
+                if (nextChar == '.') {
+                    addChar(); // Include the decimal point
+                    getChar();
+                    while (charClass == DIGIT) {
+                        addChar();
+                        getChar();
+                    }
+                    nextToken = FLOAT_LIT;
+                } else if (Character.isLetter(nextChar) || nextChar == '_') {
+                    while (Character.isLetter(nextChar) || charClass == DIGIT || nextChar == '_') {
+                        addChar();
+                        getChar();
+                    }
+                    error = new StringBuilder("Error - illegal identifier");
+                    nextToken = EOF;
+                } else {
+                    nextToken = INT_LIT;
+                }
+                break;
+            case '\"':
+                lookup(nextChar);
+                getChar();
+                break;
+            /* Operators and punctuation */
+            case UNKNOWN:
+                lookup(nextChar);
+                getChar();
+                break;
+            /* EOF */
+            case EOF:
+                nextToken = EOF;
+                lexeme = new StringBuilder("EOF");
+                System.out.println("here");
+                break;
+        }
+        /* End of switch */
+        System.out.printf("Next token is: %d, Next lexeme is %s", nextToken, lexeme);
+        System.out.printf("\t%s\n", error);
+    }
+}
---
+++ SyntaxAnalyzer.java
@@ -0,0 +1,317 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Stack;
+
+public class SyntaxAnalyzer {
+
+    private static Stack<Integer> stack = new Stack<>();
+    private static boolean expectDataTypeOrIdent = true;
+    //private static boolean expectOperandOrDataType = true;
+    private static boolean expectOperandOrDataType = false;
+    private static boolean expectOperator = false;
+    //private static int lineNumber = 1;
+    private static boolean expectsemiColum = false;
+    private static boolean alreadyincrement = false;
+    private static boolean   inparent=false ;
+     private static boolean inforCond=false  ;
+     public static boolean stackempty=true;
+     public static boolean openparen=true;
+    public static void main(String[] args) {
+        try {
+            LexicalAnalyzer.in_fp = new BufferedReader(new FileReader("input20.txt"));
+            LexicalAnalyzer.getChar();
+
+            do {
+                LexicalAnalyzer.lex();
+                int token = LexicalAnalyzer.nextToken;
+                System.out.println("string "+token+'\n');
+                //System.out.println("new line"+LexicalAnalyzer.lineNum);
+                  
+
+                if (token == LexicalAnalyzer.EOF) {
+                    break;
+                }
+                
+                System.out.println('x');
+                
+
+                switch (token) {
+
+                    case LexicalAnalyzer.LEFT_BRACE:
+                    case LexicalAnalyzer.LEFT_BRACKET:
+                    case LexicalAnalyzer.LEFT_PAREN:inparent=true ;openparen=false;
+                    
+                    
+                        stack.push(token);
+                        System.out.println(stack.lastElement());
+                        expectDataTypeOrIdent = true;
+                        expectOperandOrDataType = false;
+                        expectOperator = false;
+                        expectsemiColum = false;
+                        break;
+                    case LexicalAnalyzer.RIGHT_PAREN:
+                    if (stack.isEmpty() || stack.pop() != LexicalAnalyzer.LEFT_PAREN) {
+                            System.err.println("Error - Unmatched )" + token + " at line " + LexicalAnalyzer.lineNum);
+                            
+                            return;
+                        }
+                        expectDataTypeOrIdent = true;
+                        expectOperandOrDataType = false;
+                        expectOperator = false;
+                        expectsemiColum = false;
+                        inparent =false ;
+                        inforCond=false ;
+                        
+                        break;
+                    case LexicalAnalyzer.RIGHT_BRACE:
+                    
+                    if (stack.isEmpty() || stack.pop() != LexicalAnalyzer.LEFT_BRACE) {
+                            System.err.println("Error - Unmatched closing }"  + " at line " + LexicalAnalyzer.lineNum);
+                            System.out.println("here 2");
+                            return;
+                        }
+                       System.out.println("here");
+                        expectDataTypeOrIdent = true;
+                        expectOperandOrDataType = false;
+                        expectOperator = false;
+                        expectsemiColum = false;
+                        //stack.pop();
+
+                        break;
+                    case LexicalAnalyzer.RIGHT_BRACKET:
+                        if (stack.isEmpty() || stack.pop() != LexicalAnalyzer.LEFT_BRACKET) {
+                            System.err.println("Error - Unmatched ]" + token + " at line " + LexicalAnalyzer.lineNum);
+                          
+                            return;
+                        }
+                        expectDataTypeOrIdent = true;
+                        expectOperandOrDataType = false;
+                        expectOperator = false;
+                        expectsemiColum = false;
+                        //stack.pop();
+                        break;
+                    case LexicalAnalyzer.ASSIGN_OP:
+                        if (!expectDataTypeOrIdent) { // Assign op should not come when a data type or identifier is expected.
+                            expectOperator = false;
+                            expectOperandOrDataType = true; // After an assign op, we expect an operand (literal or identifier).
+                            expectDataTypeOrIdent=false;
+                           expectsemiColum = false;
+                        } else {
+                            System.err.println("Syntax error - Unexpected assignment operator at line " + LexicalAnalyzer.lineNum);
+                            return;
+                        }
+                        break;
+                    case LexicalAnalyzer.SEMICOLON:
+                        // After a semicolon, we expect a new statement, so we should expect a data type or identifier again.
+                        if(expectOperandOrDataType ){
+                             System.err.println("Syntax error - Unexpected semi colon operator at line " + LexicalAnalyzer.lineNum);
+                            return;
+                        }
+                        expectDataTypeOrIdent = true;
+                        expectOperandOrDataType = false;
+                        expectOperator = false;
+                    
+                        expectsemiColum = false;
+                        alreadyincrement = true;
+                        break;
+                    case LexicalAnalyzer.INT_LIT:
+                    case LexicalAnalyzer.FLOAT_LIT:
+                    case LexicalAnalyzer.STR_LIT:
+                        if (expectDataTypeOrIdent) {
+                             expectOperator = true;
+                             expectDataTypeOrIdent = false;
+                             expectOperandOrDataType = false;
+                             expectsemiColum = true;
+                        } else if(expectOperator && !expectOperandOrDataType){
+                            System.err.println("Syntax error - String assignment error at line " + LexicalAnalyzer.lineNum);
+                            return;
+                        }
+                        else if(expectsemiColum && !expectOperandOrDataType){
+                            System.err.println("Syntax error - Missing semi colon at line " + LexicalAnalyzer.lineNum);
+                            return;
+
+                        }
+                        else if (!expectOperandOrDataType) {
+                            System.err.println("Syntax error - Unexpected datatype declaration at line " + LexicalAnalyzer.lineNum);
+                            return;
+                        }
+                        expectOperandOrDataType = false;
+                        expectOperator = true;
+                        expectsemiColum = true;
+                        break;
+                    case LexicalAnalyzer.IDENT:
+                         System.out.println(openparen);
+                         if(openparen){
+                            System.err.println("Syntax error - Missing open ( " + LexicalAnalyzer.lineNum);
+                            return;
+                        }
+                        if (!expectDataTypeOrIdent ) {
+                            System.err.println("Syntax error - Unexpected identifier at line " + LexicalAnalyzer.lineNum);
+                            return;
+                        }
+                        expectOperator = true;
+                        expectOperandOrDataType = false;
+                        if(inparent  && inforCond){
+                           expectDataTypeOrIdent=true ;
+                        }else {
+                            expectDataTypeOrIdent=false ;
+                        }
+                        
+                        expectsemiColum = true;
+                        
+                        break;
+                    case LexicalAnalyzer.ADD_OP:
+                    case LexicalAnalyzer.SUB_OP:
+                    case LexicalAnalyzer.MULT_OP:
+                    case LexicalAnalyzer.DIV_OP:
+                    if(openparen==true){
+                            System.err.println("Syntax error - UMissing open ) " + LexicalAnalyzer.lineNum);
+                            return;
+                        }
+                        if (!expectOperator) {
+                            System.err.println("Syntax error - Missing operand before operator at line " + LexicalAnalyzer.lineNum);
+                            return;
+                        } 
+                        else {
+                            expectOperator = false;
+                            expectOperandOrDataType = true;
+                        }
+                        break;
+                    case LexicalAnalyzer.EQUALS:
+                    if(openparen==true){
+                            System.err.println("Syntax error - UMissing open ) " + LexicalAnalyzer.lineNum);
+                            return;
+                        }
+                    if (!expectDataTypeOrIdent) { // equal op should not come when a data type or identifier is expected.
+                        expectDataTypeOrIdent=false;
+                        expectOperator = false;
+                            expectOperandOrDataType = true; // After an assign op, we expect an operand (literal or identifier).
+                        } 
+                        
+                        else {
+                            System.err.println("Syntax error - Unexpected equals operator at line " + LexicalAnalyzer.lineNum);
+                            return;
+                        }
+                        break;
+
+                    case LexicalAnalyzer.GREATER_THAN:
+                    System.out.println(expectDataTypeOrIdent);
+                    if(openparen==true){
+                            System.err.println("Syntax error - UMissing open ) " + LexicalAnalyzer.lineNum);
+                            return;
+                        }
+                    if (!expectDataTypeOrIdent) { // equal op should not come when a data type or identifier is expected.
+                        expectDataTypeOrIdent=false;
+                        expectOperator = false;
+                            expectOperandOrDataType = true; // After an assign op, we expect an operand (literal or identifier).
+                        } 
+                        else {
+                            System.err.println("Syntax error - Unexpected Greater than operator at line " + LexicalAnalyzer.lineNum);
+                            return;
+                        }
+                        break;
+
+                   
+
+                    case LexicalAnalyzer.LESS_THAN:
+                    if(openparen==true){
+                            System.err.println("Syntax error - UMissing open ) " + LexicalAnalyzer.lineNum);
+                            return;
+                        }
+                    if (!expectDataTypeOrIdent) { // equal op should not come when a data type or identifier is expected.
+                        expectDataTypeOrIdent=false;
+                        expectOperator = false;
+                            expectOperandOrDataType = true; // After an assign op, we expect an operand (literal or identifier).
+                        } 
+                        else {
+                            System.err.println("Syntax error - Unexpected less than operator at line " + LexicalAnalyzer.lineNum);
+                            return;
+                        }
+                        break;
+
+
+
+                   
+                    case  LexicalAnalyzer.FOR :
+                       if ( expectsemiColum ){
+                        System.err.println("Syntax error - Missing  semi colum at line " + LexicalAnalyzer.lineNum);
+                        return ;
+                       }
+                       openparen=true;
+                       expectOperator = false;
+                        expectOperandOrDataType = false;
+                        expectDataTypeOrIdent=false;
+                        inforCond=true ;
+                        //System.out.println("je suis venu  ici line " );
+                       break ;
+
+                       case  LexicalAnalyzer.WHILE :
+                       if ( expectsemiColum ){
+                        System.err.println("Syntax error - Missing  semi colum at line " + LexicalAnalyzer.lineNum);
+                        return ;
+                       }
+                       openparen=true;
+                       expectOperator = false;
+                        expectOperandOrDataType = false;
+                        expectDataTypeOrIdent=false;
+                        inforCond=false ;
+                        //System.out.println("je suis venu  ici line " );
+                       break ;
+
+                       case  LexicalAnalyzer.IF :
+                       if ( expectsemiColum ){
+                        System.err.println("Syntax error - Missing  semi colum at line " + LexicalAnalyzer.lineNum);
+                        return ;
+                       }
+                       openparen=true;
+                       expectOperator = false;
+                        expectOperandOrDataType = false;
+                        expectDataTypeOrIdent=false;
+                        inforCond=false ;
+                        //System.out.println("je suis venu  ici line " );
+                       break ;
+
+                    //case 
+                    case LexicalAnalyzer.COMMENT:
+                       if (!alreadyincrement && !expectsemiColum){
+                          
+                             //System.out.println("comment line "+lineNumber);
+                       }
+                       
+                        
+                        break;
+
+                    
+                }
+                // for (int i=0;i<stack.size();i++){
+                //     System.out.println(stack.indexOf(i));
+                // }
+                System.out.println("here3");
+                
+            } while (LexicalAnalyzer.nextToken != LexicalAnalyzer.EOF);
+            //while(true);
+        for (int i=0;i<stack.size();i++){
+                  System.out.println(stack.indexOf(i));
+                 }
+
+            // if (!stack.isEmpty()) {
+            //     System.err.println("Error - Unmatched symbol " + stack.pop() + "' at the end of code");
+            // } else {
+            //     System.out.println("Syntax analysis completed successfully!");
+            // }
+            
+            while (!stack.isEmpty()) {
+                int unmatchedToken = stack.pop();
+                System.err.println("Error - Unmatched symbol " + unmatchedToken+" at the end of code  failed");
+                stackempty=false;
+            }
+
+            if (stackempty ) {
+                System.out.println("Syntax analysis completed successfully!");
+            }
+        } catch (IOException e) {
+            System.err.println("ERROR - cannot open input11.txt");
+        }
+    }
+}
